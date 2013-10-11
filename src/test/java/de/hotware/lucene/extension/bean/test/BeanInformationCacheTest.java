39aa9340ff370d322ea46b898636e65a9af02961
Martin Braun <martinbraun123@aol.com>
2013-10-11 16:12:11 +0200

    minor

--- src/test/java/de/hotware/lucene/extension/bean/test/BeanInformationCacheTest.java
+++ src/test/java/de/hotware/lucene/extension/bean/test/BeanInformationCacheTest.java
@@ -1,5 +1,10 @@
 package de.hotware.lucene.extension.bean.test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.de.GermanAnalyzer;
 import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
 
 import de.hotware.lucene.extension.bean.BeanField;
@@ -13,25 +18,40 @@ public class BeanInformationCacheTest extends TestCase {
 
 	public final class AnalyzerTestBean {
 
-		//not being serialized -> exception is expected when used with the BeanConverter
+		// not being serialized -> exception is expected when used with the
+		// BeanConverter
 		@BeanField(store = true, index = true, type = StockType.StringType.class, analyzerProvider = StockAnalyzerProvider.GermanAnalyzerProvider.class)
 		public String test;
 
 	}
-	
+	@SuppressWarnings("unchecked")
+	public static <T extends Analyzer> Class<T> getAnalyzer(
+			PerFieldAnalyzerWrapper wrapper, String name, Class<T> analyzer)
+			throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		Method getterMethod = wrapper.getClass().getDeclaredMethod(
+				"getWrappedAnalyzer", String.class);
+		getterMethod.setAccessible(true);
+		return (Class<T>) getterMethod.invoke(wrapper, name).getClass();
+	}
+
 	public void testBeanInformationCacheImpl() {
 		new BeanInformationCacheImpl();
 	}
-	
+
 	public void testGetFieldInformations() {
 		BeanInformationCache cache = new BeanInformationCacheImpl();
-		cache.getFieldInformations(AnalyzerTestBean.class);		
+		cache.getFieldInformations(AnalyzerTestBean.class);
 	}
-	
-	public void testGetPerFieldAnalyzerWrapper() {
+
+	public void testGetPerFieldAnalyzerWrapper() throws NoSuchMethodException,
+			SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
 		BeanInformationCache cache = new BeanInformationCacheImpl();
-		PerFieldAnalyzerWrapper wrapper = cache.getPerFieldAnalyzerWrapper(AnalyzerTestBean.class);
-		assertTrue(wrapper.toString().contains("GermanAnalyzer"));
+		PerFieldAnalyzerWrapper wrapper = cache
+				.getPerFieldAnalyzerWrapper(AnalyzerTestBean.class);
+
+		assertEquals(GermanAnalyzer.class, getAnalyzer(wrapper, "test", GermanAnalyzer.class));
 	}
 
 }
